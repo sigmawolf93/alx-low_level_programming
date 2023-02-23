e207a7846f21544bfaf56280731352cb1257a433
sigmawolf <mercyoladipupo93@gmail.com>
2023-02-23 14:31:45 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
@@ -17,3 +15,4 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+
