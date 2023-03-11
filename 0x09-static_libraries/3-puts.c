219ccfe8cb1549d74ba2818e057c59c52f10714e
sigmawolf <mercyoladipupo93@gmail.com>
2023-03-10 16:21:11 -0800

    wahala

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,27 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
 
+/**
+ * _puts - function declaration
+ * @str: string pointer
+ * Return: void
+ */
+
+void _puts(char *str);
+
+/**
+ * _puts - function definition
+ * Description: prints a string
+ * @str: the string pointer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; *(str + index) != '\0'; index++)
+	{
+		_putchar(*(str + index));
+	}
+	_putchar('\n');
+}
